667c1553cad1e1717b7e971fbdd2325b370b438d
sabababluani <Saba.bablo@gmail.com>
2025-09-26 02:05:13 +0400

    Initialize NestJS project with essential configurations and structure. Added .gitignore, ESLint, Prettier, and TypeScript configurations. Created basic application structure including controllers, services, and modules for user management. Implemented initial tests and setup for Jest. Updated package.json with dependencies and scripts for building, testing, and running the application.

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
