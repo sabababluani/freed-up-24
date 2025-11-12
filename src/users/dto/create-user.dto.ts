10ce507ddc052f4dcffa697e8356b9548ff7abde
sabababluani <Saba.bablo@gmail.com>
2025-11-12 23:20:01 +0400

    changed class validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsNumber, IsOptional, IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
@@ -16,7 +16,7 @@ export class CreateUserDto {
   @IsString()
   password: string;
 
-  @IsNumber()
+  @IsString()
   @IsOptional()
   money?: string;
 }
